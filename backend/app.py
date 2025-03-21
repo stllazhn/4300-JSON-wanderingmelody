9e1c0c8c0700af3bebfd3578dbee77fe6cae8a26
Andisha <as3254@cornell.edu>
2025-03-21 16:25:58 -0400

    changes the local host website

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 # Function to check if Streamlit is running
 def check_streamlit():
     try:
-        response = requests.get("http://localhost:8501")
+        response = requests.get("http://10.48.49.54:8501")
         return response.status_code == 200
     except requests.ConnectionError:
         return False
@@ -19,7 +19,7 @@ def run_streamlit():
     subprocess.run(["streamlit", "run", "streamlit_app.py"])
 
 # Start Streamlit in the background and ensure it starts before rendering Flask page
-@app.before_first_request
+@app.before_request
 def before_first_request():
     # Start Streamlit in a separate thread
     threading.Thread(target=run_streamlit, daemon=True).start()
@@ -36,3 +36,8 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)  # `use_reloader=False` to avoid restarting Flask server when Streamlit starts
+
+
+## currently the html file is loading form the localhost port 8501, 
+# and then you should change it to the server backend. 
+# the server backend can be found in the server site and not the local host.
