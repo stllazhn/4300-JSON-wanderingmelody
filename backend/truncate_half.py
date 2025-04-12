479604783433ce2a326d9e07e4d5615dd2ea26b1
Stella Zhang <jz766@cornell.edu>
2025-04-12 16:16:01 -0400

    half of json file to reduce memory

---
+++ backend/truncate_half.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+df = pd.read_json('spotify_millsongdata.json')
+
+word_counts = df['text'].apply(lambda x: len(str(x).split()))
+median_word_count = int(word_counts.median())
+
+def truncate_words(text, word_limit):
+    words = str(text).split()
+    if len(words) <= word_limit:
+        return text
+    return ' '.join(words[:word_limit])
+
+df['text'] = df['text'].apply(lambda x: truncate_words(x, median_word_count))
+
+# Select the first half of the DataFrame
+half_df = df.iloc[:len(df)//2]
+
+# Save to JSON
+half_df.to_json('spotify_millsongdata_trunc_half.json', orient='records')
