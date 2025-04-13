565f0ed334d0f93a7ecab7e94089c8c9854820c7
Stella Zhang <jz766@cornell.edu>
2025-04-13 13:12:41 -0400

    someone used the wrong json file, again

--- backend/ml.py
+++ backend/ml.py
@@ -68,15 +68,8 @@ custom_stopwords = set([
 # Load datasets
 #spotify_df = pd.read_csv("mini_spotify_track_db.csv")
 #spotify_df = pd.read_json("spotify-tracks-dataset.json")
-lyric_df = pd.read_json("shortened_spotify.json")
+#lyric_df = pd.read_json("shortened_spotify.json")
 #lyric_df = pd.read_csv("mini_spotify_db.csv")
-
-def polarity_scores_for_songs(df):
-    sia = SentimentIntensityAnalyzer() 
-    df['sentiment'] = df['text'].apply(lambda song_lyrics: sia.polarity_scores(song_lyrics)['compound'])
-
-print("Lyrics Dataset Sample:")
-print(lyric_df.head())
 
 # Tokenization function
 def tokenize(text: str) -> List[str]:
