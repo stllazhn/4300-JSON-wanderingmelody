3df05511ce03f0d0eae3ff6ff36953501d8f20e0
Stella Zhang <jz766@cornell.edu>
2025-04-13 13:16:20 -0400

    add lyric_df

--- backend/ml.py
+++ backend/ml.py
@@ -68,7 +68,7 @@ custom_stopwords = set([
 # Load datasets
 #spotify_df = pd.read_csv("mini_spotify_track_db.csv")
 #spotify_df = pd.read_json("spotify-tracks-dataset.json")
-#lyric_df = pd.read_json("shortened_spotify.json")
+lyric_df = pd.read_json("shortened_spotify.json")
 #lyric_df = pd.read_csv("mini_spotify_db.csv")
 
 # Tokenization function
